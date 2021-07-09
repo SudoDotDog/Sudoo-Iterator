c5a1a58ec64edf770bcbacff4583d7b6bebf2ad7
WMXPY <wm@mengw.io>
2021-07-09 16:26:13 -0500

    finsih tests

--- test/unit/list/batch.test.ts
+++ test/unit/list/batch.test.ts
@@ -28,4 +28,40 @@ describe('Given {BatchIterator} class', (): void => {
         expect(iterator.count).to.be.equal(0);
         expect(iterator.nextLeft).to.be.equal(2);
     });
+
+    it('should be able to calculate length - ceil', (): void => {
+
+        const elements: number[] = [
+            chance.natural(),
+            chance.natural(),
+            chance.natural(),
+            chance.natural(),
+        ];
+        const iterator: BatchIterator<number> = BatchIterator.create(elements, 3);
+
+        expect(iterator).to.be.lengthOf(2);
+        expect(iterator.hasNext()).to.be.true;
+        expect(iterator.count).to.be.equal(0);
+        expect(iterator.nextLeft).to.be.equal(2);
+    });
+
+    it('should be able to get batch elements', (): void => {
+
+        const elements: number[] = [
+            chance.natural(),
+            chance.natural(),
+            chance.natural(),
+            chance.natural(),
+        ];
+        const iterator: BatchIterator<number> = BatchIterator.create(elements, 3);
+
+        const batches: number[][] = [];
+        for (const batch of iterator) {
+            batches.push(batch);
+        }
+
+        expect(batches).to.be.lengthOf(2);
+        expect(batches[0]).to.be.lengthOf(3);
+        expect(batches[1]).to.be.lengthOf(1);
+    });
 });
